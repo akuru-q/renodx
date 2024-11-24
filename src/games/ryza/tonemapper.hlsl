64eb28f16d7544809c773be2dd76f457aefc4f8d
akuru-q <115506513+akuru-q@users.noreply.github.com>
2024-11-24 19:53:27 +0100

    fix(ryza): switch to new config style

--- src/games/ryza/tonemapper.hlsl
+++ src/games/ryza/tonemapper.hlsl
@@ -28,24 +28,26 @@ float3 applyUserTonemap(float3 untonemapped, float3 vanillaColor, float midGray)
     float renoDRTSaturation = 1.f; //
     float renoDRTHighlights = 1.f;
 
-    renodx::tonemap::Config config = renodx::tonemap::config::Create(
-      injectedData.toneMapType,
-      injectedData.toneMapPeakNits,
-      injectedData.toneMapGameNits,
-      1,
-      injectedData.colorGradeExposure,
-      injectedData.colorGradeHighlights,
-      injectedData.colorGradeShadows,
-      injectedData.colorGradeContrast,
-      injectedData.colorGradeSaturation,
-      vanillaMidGray,
-      vanillaMidGray * 100.f,
-      renoDRTHighlights,
-      renoDRTShadows,
-      renoDRTContrast,
-      renoDRTSaturation,
-      renoDRTDechroma,
-      renoDRTFlare);
+    renodx::tonemap::Config config = renodx::tonemap::config::Create();
+    
+    config.type = injectedData.toneMapType;
+    config.peak_nits = injectedData.toneMapPeakNits;
+    config.game_nits = injectedData.toneMapGameNits;
+    config.gamma_correction = 1;
+    config.exposure = injectedData.colorGradeExposure;
+    config.highlights = injectedData.colorGradeHighlights;
+    config.shadows = injectedData.colorGradeShadows;
+    config.contrast = injectedData.colorGradeContrast;
+    config.saturation = injectedData.colorGradeSaturation;
+    
+    config.reno_drt_highlights = renoDRTHighlights;
+    config.reno_drt_shadows = renoDRTShadows;
+    config.reno_drt_contrast = renoDRTContrast;
+    config.reno_drt_saturation = renoDRTSaturation;
+    config.reno_drt_dechroma = renoDRTDechroma;
+    config.mid_gray_value = vanillaMidGray;
+    config.mid_gray_nits = vanillaMidGray * 100.f;
+    config.reno_drt_flare = renoDRTFlare;
 
     outputColor = renodx::tonemap::config::Apply(outputColor, config);
     
