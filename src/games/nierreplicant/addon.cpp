9a90000f50d290f3142be6d5013459d09771d4e3
akuru-q <115506513+akuru-q@users.noreply.github.com>
2024-12-29 12:15:18 +0100

    fix(nierreplicant): fix swapchain proxy

--- src/games/nierreplicant/addon.cpp
+++ src/games/nierreplicant/addon.cpp
@@ -294,14 +294,8 @@ BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
       renodx::mods::swapchain::prevent_full_screen = false;
       renodx::mods::shader::force_pipeline_cloning = true;
 
-      renodx::mods::swapchain::swap_chain_proxy_vertex_shader = {
-          _swap_chain_proxy_vertex_shader,
-          _swap_chain_proxy_vertex_shader + sizeof(_swap_chain_proxy_vertex_shader),
-      };
-      renodx::mods::swapchain::swap_chain_proxy_pixel_shader = {
-          _swap_chain_proxy_pixel_shader,
-          _swap_chain_proxy_pixel_shader + sizeof(_swap_chain_proxy_pixel_shader),
-      };
+      renodx::mods::swapchain::swap_chain_proxy_vertex_shader = __swap_chain_proxy_vertex_shader;
+      renodx::mods::swapchain::swap_chain_proxy_pixel_shader = __swap_chain_proxy_pixel_shader;
 
       renodx::mods::swapchain::swap_chain_upgrade_targets.push_back({
           .old_format = reshade::api::format::r10g10b10a2_typeless,
