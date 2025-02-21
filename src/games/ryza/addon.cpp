757a226955ad3b5139d0567c65e93d7885ff4c60
akuru-q <115506513+akuru-q@users.noreply.github.com>
2025-02-21 10:38:45 +0100

    fix(ryza): update reshade

--- src/games/ryza/addon.cpp
+++ src/games/ryza/addon.cpp
@@ -390,7 +390,7 @@ void OnDestroyDevice(reshade::api::device* device) {
   device->destroy_private_data<DeviceData>();
 }
 
-void OnInitSwapchain(reshade::api::swapchain* swapchain) {
+void OnInitSwapchain(reshade::api::swapchain* swapchain, bool resize) {
   auto device = swapchain->get_device();
   auto& data = device->get_private_data<DeviceData>();
 
@@ -424,7 +424,7 @@ void OnInitSwapchain(reshade::api::swapchain* swapchain) {
   }
 }
 
-void OnDestroySwapchain(reshade::api::swapchain* swapchain) {
+void OnDestroySwapchain(reshade::api::swapchain* swapchain, bool resize) {
   auto device = swapchain->get_device();
   auto& data = device->get_private_data<DeviceData>();
 
