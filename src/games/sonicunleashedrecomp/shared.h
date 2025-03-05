067447aee78d57f7963f229b126908c06cfe09be
akuru-q <115506513+akuru-q@users.noreply.github.com>
2025-03-05 11:00:55 +0100

    feat(sonicunleashedrecomp): add frostbite, shoulder start

--- src/games/sonicunleashedrecomp/shared.h
+++ src/games/sonicunleashedrecomp/shared.h
@@ -6,7 +6,7 @@
 #define RENODX_DIFFUSE_WHITE_NITS            RENODX_GRAPHICS_WHITE_NITS
 #define RENODX_TONE_MAP_TYPE                 shader_injection.tone_map_type
 #define RENODX_RENO_DRT_TONE_MAP_METHOD      renodx::tonemap::renodrt::config::tone_map_method::REINHARD
-#define RENODX_RENO_DRT_WHITE_CLIP           6.f
+#define RENODX_RENO_DRT_WHITE_CLIP           16.f - (CUSTOM_TONE_MAP_SHOULDER_START * 16.f)
 #define RENODX_TONE_MAP_EXPOSURE             shader_injection.tone_map_exposure
 #define RENODX_TONE_MAP_HIGHLIGHTS           shader_injection.tone_map_highlights
 #define RENODX_TONE_MAP_SHADOWS              shader_injection.tone_map_shadows
@@ -19,13 +19,14 @@
 #define RENODX_TONE_MAP_HUE_SHIFT            shader_injection.tone_map_hue_shift
 #define RENODX_TONE_MAP_HUE_SHIFT_METHOD     HUE_SHIFT_METHOD_SDR_MODIFIED
 #define RENODX_TONE_MAP_HUE_SHIFT_MODIFIER   0.5f
-#define RENODX_TONE_MAP_WORKING_COLOR_SPACE  shader_injection.tone_map_working_color_space
+#define RENODX_TONE_MAP_WORKING_COLOR_SPACE  renodx::color::convert::COLOR_SPACE_BT709 //shader_injection.tone_map_working_color_space
 #define RENODX_TONE_MAP_HUE_PROCESSOR        shader_injection.tone_map_hue_processor
 #define RENODX_TONE_MAP_PER_CHANNEL          shader_injection.tone_map_per_channel
 #define RENODX_GAMMA_CORRECTION              shader_injection.gamma_correction
-#define CUSTOM_TONE_MAP_CONFIGURATION        shader_injection.custom_tone_map_configuration
-#define CUSTOM_HDR_VIDEOS                    shader_injection.custom_hdr_videos
-#define CUSTOM_HDR_VIDEOS_PEAK_NITS          shader_injection.custom_hdr_videos_peak_nits
+//#define CUSTOM_TONE_MAP_CONFIGURATION        shader_injection.custom_tone_map_configuration
+//#define CUSTOM_HDR_VIDEOS                    shader_injection.custom_hdr_videos
+//#define CUSTOM_HDR_VIDEOS_PEAK_NITS          shader_injection.custom_hdr_videos_peak_nits
+#define CUSTOM_TONE_MAP_SHOULDER_START       shader_injection.custom_tone_map_shoulder_start
 
 // Must be 32bit aligned
 // Should be 4x32
@@ -48,8 +49,9 @@ struct ShaderInjectData {
   float tone_map_hue_processor;
   float tone_map_per_channel;
   float gamma_correction;
-  float custom_hdr_videos;
-  float custom_hdr_videos_peak_nits;
+  //float custom_hdr_videos;
+  //float custom_hdr_videos_peak_nits;
+  float custom_tone_map_shoulder_start;
 };
 
 #ifndef __cplusplus
--- src/games/sonicunleashedrecomp/swap_chain_proxy_pixel_shader.ps_6_0.hlsl
+++ src/games/sonicunleashedrecomp/swap_chain_proxy_pixel_shader.ps_6_0.hlsl
@@ -1,11 +1,17 @@
 #include "./shared.h"
-#include "./DICE.hlsl"
 
 float3 applyRenoDice(float3 color) {
   const float paperWhite = RENODX_DIFFUSE_WHITE_NITS / renodx::color::srgb::REFERENCE_WHITE;
   const float peakWhite = RENODX_PEAK_WHITE_NITS / renodx::color::srgb::REFERENCE_WHITE;
-  const float highlightsShoulderStart = paperWhite;  // Don't tonemap the "SDR" range (in luminance), we want to keep it looking as it used to look in SDR
+  const float highlightsShoulderStart = CUSTOM_TONE_MAP_SHOULDER_START;
   return renodx::tonemap::dice::BT709(color.rgb * paperWhite, peakWhite, highlightsShoulderStart) / paperWhite;
+}
+
+float3 applyRenoFrostbite(float3 color) {
+  const float paperWhite = RENODX_DIFFUSE_WHITE_NITS / renodx::color::srgb::REFERENCE_WHITE;
+  const float peakWhite = RENODX_PEAK_WHITE_NITS / renodx::color::srgb::REFERENCE_WHITE;
+  const float highlightsShoulderStart = CUSTOM_TONE_MAP_SHOULDER_START;
+  return renodx::tonemap::frostbite::BT709(color.rgb * paperWhite, peakWhite, highlightsShoulderStart, RENODX_TONE_MAP_HIGHLIGHT_SATURATION, RENODX_TONE_MAP_HUE_CORRECTION) / paperWhite;
 }
 
 Texture2D t0 : register(t0);
@@ -15,6 +21,7 @@ float4 main(float4 vpos: SV_POSITION, float2 uv: TEXCOORD0)
   float4 SV_Target = t0.Sample(s0, uv);
 
   if (RENODX_TONE_MAP_TYPE == 3.f) {
+    SV_Target.rgb = renodx::color::bt709::clamp::AP1(SV_Target.rgb);
     SV_Target.rgb = renodx::draw::ToneMapPass(SV_Target.rgb);
   } else if (RENODX_TONE_MAP_TYPE == 6.f) {
     renodx::tonemap::Config config = renodx::tonemap::config::Create();
@@ -31,11 +38,28 @@ float4 main(float4 vpos: SV_POSITION, float2 uv: TEXCOORD0)
     SV_Target.rgb = applyRenoDice(SV_Target.rgb);
 
     SV_Target.rgb = renodx::color::correct::Hue(SV_Target.rgb, saturate(SV_Target.rgb), RENODX_TONE_MAP_HUE_SHIFT, RENODX_TONE_MAP_WORKING_COLOR_SPACE);
+  } else if (RENODX_TONE_MAP_TYPE == 9.f) {
+    renodx::tonemap::Config config = renodx::tonemap::config::Create();
+    config.exposure = RENODX_TONE_MAP_EXPOSURE;
+    config.highlights = RENODX_TONE_MAP_HIGHLIGHTS;
+    config.shadows = RENODX_TONE_MAP_SHADOWS;
+    config.contrast = RENODX_TONE_MAP_CONTRAST;
+    config.saturation = RENODX_TONE_MAP_SATURATION;
+    config.reno_drt_dechroma = RENODX_TONE_MAP_BLOWOUT;
+    config.hue_correction_strength = RENODX_TONE_MAP_HUE_CORRECTION;
+
+    SV_Target.rgb = renodx::color::grade::UserColorGrading(SV_Target.rgb, config.exposure, config.highlights, config.shadows, config.contrast, config.saturation);
+
+    SV_Target.rgb = applyRenoFrostbite(SV_Target.rgb);
+
+    SV_Target.rgb = renodx::color::correct::Hue(SV_Target.rgb, saturate(SV_Target.rgb), RENODX_TONE_MAP_HUE_SHIFT, RENODX_TONE_MAP_WORKING_COLOR_SPACE);
   } else {
     SV_Target.rgb = saturate(SV_Target.rgb);
   }
 
   SV_Target = renodx::draw::SwapChainPass(SV_Target);
+
+  SV_Target.rgb = renodx::color::bt709::clamp::AP1(SV_Target.rgb);
 
   return SV_Target;
 }
