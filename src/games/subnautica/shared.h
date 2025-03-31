644a77e92cccb362831c78d659c9b4d26d96368f
akuru-q <115506513+akuru-q@users.noreply.github.com>
2025-03-31 18:25:39 +0200

    fix(subnautica): fix UI

--- src/games/subnautica/shared.h
+++ src/games/subnautica/shared.h
@@ -21,6 +21,8 @@
 #define RENODX_TONE_MAP_HUE_PROCESSOR        shader_injection.tone_map_hue_processor
 #define RENODX_TONE_MAP_PER_CHANNEL          shader_injection.tone_map_per_channel
 #define RENODX_GAMMA_CORRECTION              shader_injection.gamma_correction
+#define RENODX_INTERMEDIATE_ENCODING         0.f
+#define RENODX_SWAP_CHAIN_DECODING           0.f
 //#define CUSTOM_BLOOM                         shader_injection.custom_bloom
 
 // Must be 32bit aligned
