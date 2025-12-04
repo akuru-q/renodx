a02bac62c5d81ae11eaab0486e16b50e360a73e5
akuru-q <115506513+akuru-q@users.noreply.github.com>
2025-12-04 23:05:04 +0100

    feat(yakuza0): remove exp rolloff

--- src/games/yakuza0/common.hlsl
+++ src/games/yakuza0/common.hlsl
@@ -133,48 +133,8 @@ float3 InvRenoDRT(float3 color) {
   return renodx::tonemap::renodrt::BT709(untonemapped, hdr_video_config);
 }
 
-float3 ExponentialRollOffByLum(float3 color, float output_luminance_max, float highlights_shoulder_start = 0.f) {
-  const float source_luminance = renodx::color::y::from::BT709(color);
-
-  [branch]
-  if (source_luminance > 0.0f) {
-    const float compressed_luminance = renodx::tonemap::ExponentialRollOff(source_luminance, highlights_shoulder_start, output_luminance_max);
-    color *= compressed_luminance / source_luminance;
-  }
-
-  return color;
-}
-
-float3 ApplyExponentialRollOff(float3 color) {
-  const float paperWhite = RENODX_DIFFUSE_WHITE_NITS / renodx::color::srgb::REFERENCE_WHITE;
-
-  const float peakWhite = RENODX_PEAK_WHITE_NITS / renodx::color::srgb::REFERENCE_WHITE;
-
-  // const float highlightsShoulderStart = paperWhite;
-  const float highlightsShoulderStart = 1.f;
-
-  [branch]
-  if (RENODX_TONE_MAP_PER_CHANNEL == 0.f) {
-    return ExponentialRollOffByLum(color * paperWhite, peakWhite, highlightsShoulderStart) / paperWhite;
-  } else {
-    return renodx::tonemap::ExponentialRollOff(color * paperWhite, highlightsShoulderStart, peakWhite) / paperWhite;
-  }
-}
-
 float3 ApplyRenoTonemap(float3 color) {
-  [branch]
-  if (RENODX_TONE_MAP_TYPE == 6.f) {
-    renodx::draw::Config draw_config = renodx::draw::BuildConfig();
-    draw_config.peak_white_nits = 10000.f;
-    draw_config.tone_map_type = 3.f;
-    draw_config.tone_map_hue_correction = 0.f;
-    draw_config.tone_map_hue_shift = 0.f;
-    draw_config.tone_map_per_channel = 0.f;
-
-    color = renodx::draw::ToneMapPass(g_untonemapped, color, draw_config);
-    color = ApplyExponentialRollOff(color);
-    color = ApplyPerChannelBlowoutHueShift(color);
-  } else if (RENODX_TONE_MAP_TYPE == 3.f) {
+  if (RENODX_TONE_MAP_TYPE != 0.f) {
     renodx::draw::Config draw_config = renodx::draw::BuildConfig();
     draw_config.tone_map_hue_correction = 0.f;
     draw_config.tone_map_hue_shift = 0.f;
